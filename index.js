3f596ebd20c5c8c5710a59ae6070283fa844eabe
Eddy Matheson <rmatheson95@icloud.com>
2023-03-20 13:45:17 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Edward";
+const firstInitial = currentUser[0];
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${firstInitial}!`;
